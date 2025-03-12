aa2f790c787443b94f125de3409c30fb7cd39f7c
RameshEdirisinghe <e2145434@bit.uom.lk>
2025-03-13 02:52:03 +0530

    change dependency version

--- src/main/java/edu/ramesh/org/controller/MainController.java
+++ src/main/java/edu/ramesh/org/controller/MainController.java
@@ -1,16 +1,22 @@
 package edu.ramesh.org.controller;
 
 import edu.ramesh.org.dto.Book;
-import org.springframework.http.StreamingHttpOutputMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MainController {
 
+    private final KafkaTemplate<String,Book> kafkaTemplate;
+    private static final String Topic ="NewTopic";
 
+    @PostMapping("/msg")
     public String testMessage(@RequestBody Book book){
-
-        return null;
+        kafkaTemplate.send(Topic,book);
+        return "Published Successfully!";
     }
 }
