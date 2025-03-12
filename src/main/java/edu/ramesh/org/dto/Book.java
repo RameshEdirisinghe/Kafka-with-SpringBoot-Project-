642fbfb405d8650fbb6785de20882c89add4e7d5
RameshEdirisinghe <e2145434@bit.uom.lk>
2025-03-13 03:57:57 +0530

    update dto

--- src/main/java/edu/ramesh/org/dto/Book.java
+++ src/main/java/edu/ramesh/org/dto/Book.java
@@ -1,4 +1,15 @@
 package edu.ramesh.org.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Book {
+    private String bookName;
+    private String isbn;
 }
